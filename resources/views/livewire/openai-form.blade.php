6cc7ddacae33dac50c9b09c617cf9c684018cecf
Grandejosh <grande_josh@odontologos.com>
2023-01-06 15:01:07 -0500

    .
    .

--- resources/views/livewire/openai-form.blade.php
+++ resources/views/livewire/openai-form.blade.php
@@ -1,10 +1,12 @@
 <div>
     <h1>Open AI</h1>
+    <form action="">
     <label for="texto">Escribe tu frase</label>
     <br>
-    <textarea wire:model.defer="frase_text" id="texto" cols="30" rows="10" class="form-control"></textarea>
+    <input type="text" wire:model.defer="frase_text" id="texto" cols="30" rows="10" class="form-control">
     <br>
     <button wire:click="getOpenAI" type="button">Consultar</button>
     <br><br>
+</form>
     <p>{{ $result_text }}</p> 
 </div
