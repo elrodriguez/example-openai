b036464df77ff712431625ac8022e219a3bcc84b
elrodriguez <elrodriguez2423@gmail.com>
2023-01-06 14:11:48 -0500

    el,er

--- app/Http/Livewire/OpenaiForm.php
+++ app/Http/Livewire/OpenaiForm.php
@@ -21,9 +21,9 @@ class OpenaiForm extends Component
         $apiKey = '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';
         $result = OpenAI::completions()->create([
             'model' => 'text-davinci-003',
-            'prompt' => $this->frase_text,
+            'prompt' => 'PHP is',
         ]);
-
+        dd($result['choices'][0]['text']);
         $this->result_text = $result['choices'][0]['text'];
     }
 }
