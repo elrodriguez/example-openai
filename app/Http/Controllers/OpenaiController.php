072aa0259aadb0f1978f3562dbf4b3f4c0737511
Grandejosh <grande_josh@odontologos.com>
2023-01-06 16:16:20 -0500

    s

--- app/Http/Controllers/OpenaiController.php
+++ app/Http/Controllers/OpenaiController.php
@@ -9,18 +9,16 @@ class OpenaiController extends Controller
 {
     public function getOpenai(Request $request)
     {
-        // $consulta = $request->input('frase_text');
-        // dd($consulta);
-        // $result = OpenAI::completions()->create([
-        //     'model' => 'text-davinci-003',
-        //     'prompt' => $consulta,
-        // ]);
-        // //dd($result['choices'][0]['text']);
+        $consulta = $request->input('frase_text');
+        dd($consulta);
+        $result = OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => $consulta,
+        ]);
+        //dd($result['choices'][0]['text']);
 
-        // $result_text = $result['choices'][0]['text'];
+        $result_text = $result['choices'][0]['text'];
 
-        // dd($result_text);
-
-        dd('hola mundo');
+        dd($result_text);
     }
 }
