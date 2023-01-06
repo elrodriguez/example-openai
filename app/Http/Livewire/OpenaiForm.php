dfabb39aa613b8f54e99ee4e2fbe627786b047d8
elrodriguez <elrodriguez2423@gmail.com>
2023-01-06 14:43:59 -0500

    elmer

--- app/Http/Livewire/OpenaiForm.php
+++ app/Http/Livewire/OpenaiForm.php
@@ -7,26 +7,29 @@ use OpenAI\Laravel\Facades\OpenAI;
 
 class OpenaiForm extends Component
 {
-    public $result_text;
+    public $result_text = 'resultado';
     public $frase_text;
 
     public function mount()
     {
-        $this->getOpenAI();
+        //$this->getOpenAI();
     }
     public function render()
     {
         return view('livewire.openai-form');
     }
-
+    public function clickEE()
+    {
+        $this->result_text = $this->frase_text;
+    }
     public function getOpenAI()
     {
         $apiKey = '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';
         $result = OpenAI::completions()->create([
             'model' => 'text-davinci-003',
-            'prompt' => 'PHP is',
+            'prompt' => $this->frase_text,
         ]);
-        dd($result['choices'][0]['text']);
+        //dd($result['choices'][0]['text']);
         $this->result_text = $result['choices'][0]['text'];
     }
 }
